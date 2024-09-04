140aba534df552b5ac66d513dee14c0ce42511db
karthikpraba <kprabakaran@jackhenry.com>
2024-09-03 20:45:34 -0500

    Updated to use Link

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,7 @@ import PartyV from "./images/PartyV.png";
 import FishV from "./images/FishV.png";
 import Bar from "./images/bar.png";
 import ReactGA from "react-ga";
+import { Link } from "react-router-dom";
 
 import {
   motion,
@@ -97,7 +98,7 @@ export default function Home() {
               // Adjust positioning as needed
             />
             <li>
-            <a href="./home">Home</a>
+              <Link to="/home">Home</Link>
             </li>
             <li>
               <a href="./AboutUs">About Us</a>
